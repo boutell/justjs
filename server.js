e62ee98696b2727038eb3e5d79c81ffadf8010c1
Tom Boutell <tom@punkave.com>
2012-05-06 22:23:39 -0400

    A permalink page now exists for each post, and we have layouts and slots to reduce template code duplication and facilitate rendering nice SEO-friendly pages without resorting to JavaScript on the client side.
    Still ridiculously fast.
    
    Next I'll refactor to get rid of code duplication in the routes.

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ var app = require('express').createServer();
 
 options.templates.index = _.template(fs.readFileSync(__dirname + '/templates/index._', 'utf8'));
 options.templates.post = _.template(fs.readFileSync(__dirname + '/templates/post._', 'utf8'));
+options.templates.postBody = _.template(fs.readFileSync(__dirname + '/templates/postBody._', 'utf8'));
+options.templates.layout = _.template(fs.readFileSync(__dirname + '/templates/layout._', 'utf8'));
 
 var db;
 var postCollection;
@@ -27,13 +29,48 @@ app.get('/', function(req, res) {
     {
       throw err;
     }
-    res.send(options.templates.index({ 'posts': posts, 'options': options }));
+    var slots = {};
+    slots.body = options.templates.index({ posts: posts, options: options, slots: slots });
+    res.send(options.templates.layout({ slots: slots }));
+  });
+});
+
+app.get('/posts/:slug', function(req, res) {
+  var slug = req.params.slug;
+  postCollection.findOne({slug: slug}, function(err, post) { 
+    if (err)
+    {
+      throw err;
+    }
+    if (post)
+    {
+      var slots = {};
+      slots.body = options.templates.post({ post: post, options: options, slots: slots });
+      res.send(options.templates.layout({ slots: slots }));
+    }
+    else
+    {
+      res.status(404);
+      res.send('Post Not Found');
+    }
   });
 });
 
 db = new mongo.Db(options.db.name, new mongo.Server(options.db.host, options.db.port, {}), {});
 db.open(function(err, client) {
   postCollection = db.collection('post');
+  postCollection.ensureIndex("slug", { unique: true }, function(err, callback) 
+  {
+    if (err)
+    {
+      throw err;
+    }
+    ready();
+  });
+});
+
+function ready()
+{
   app.listen(options.http.port);  
   console.log("Listening on port " + options.http.port);
-});
+};
