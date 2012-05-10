f9f8fee7126f545bdc849873b89aa531b453fb49
Tom Boutell <tom@punkave.com>
2012-05-09 22:10:59 -0400

    * The bare beginnings of editing! We can create and save a new post. * Posts have user-friendly slugs (individual URLs). * Slugs are guaranteed to be unique - even if people are posting concurrently with the same title. * A convenient insertUniquely function is included, ready to be bolted onto any mongodb-native collection object as a method. We should share this... * The slugify function itself is nice too, although regrettably not Unicode-aware (it could be if we used xregexp)

--- server.js
+++ server.js
@@ -13,8 +13,13 @@ var options = {
 var _ = require('underscore');
 var fs = require('fs');
 var mongo = require('mongodb');
-var app = require('express').createServer();
 var async = require('async');
+var express = require('express');
+var app = express.createServer();
+
+// Use the body parser express middleware to automatically parse
+// POST form submissions
+app.use(express.bodyParser());
 
 options.templates.post = _.template(fs.readFileSync(__dirname + '/templates/post._', 'utf8'));
 options.templates.postBody = _.template(fs.readFileSync(__dirname + '/templates/postBody._', 'utf8'));
@@ -35,6 +40,8 @@ function connect(callback)
       console.log('Database initialized');
       callback(err);
     });
+    // Enhance postCollection with our insertUniquely method
+    postCollection.insertUniquely = insertUniquely;
   });
 }
 
@@ -87,6 +94,23 @@ app.get('/posts/:slug', function(req, res) {
   });
 });
 
+app.get('/new', function(req, res) {
+  sendPage(res, 'new', {});
+});
+
+app.post('/create', function(req, res) {
+  var post = _.pick(
+    _.defaults(req.body, {'title': '', 'body': ''}), 
+    'title', 'body');
+  post.slug = slugify(post.title);
+  // If there are unique index errors keep adding random digits via
+  // uniqueify until we have a unique slug. On success redirect to the
+  // index page, where we can see the new post at the top
+  postCollection.insertUniquely(post, {}, function(err, docs) {
+    res.redirect('/');
+  });
+});
+
 // Render a page template nested in the layout, allowing slots 
 // (such as overrides of the page title) to be passed back to the layout 
 function sendPage(res, template, data)
@@ -108,4 +132,66 @@ function renderPartial(template, data)
     return renderPartial(partial, partialData);
   }});
   return options.templates[template](data);
+}
+
+// Create a reasonable slug for use in URLs based on the supplied string
+function slugify(s)
+{
+  // Note: you'll need to use xregexp instead if you need non-Latin character
+  // support in slugs
+
+  // Everything not a letter or number becomes a dash
+  s = s.replace(/[^A-Za-z0-9]/g, '-');
+  // Consecutive dashes become one dash
+  s = s.replace(/\-+/g, '-');
+  // Leading dashes go away
+  s = s.replace(/^\-/, '');
+  // Trailing dashes go away
+  s = s.replace(/\-$/, '');
+  // If the string is empty, supply something so that routes still match
+  if (!s.length)
+  {
+    s = 'none';
+  }
+  return s.toLowerCase();
+}
+
+// Use like this:
+
+// mycollection.insertUniquely = insertUniquely;
+// mycollection.insertUniquely(doc, {}, callback);
+
+// You can specify additional options besides 'safe' with the
+// second parameter. 'safe' is forced on to detect unique slug errors.
+
+// If there is a conflict with another document that has the same slug, a 
+// more unique slug will automatically be generated, in a concurrency-safe way
+
+function insertUniquely(doc, options, callback)
+{
+  var self = this;
+  options.safe = true;
+  insertUniquelyBody(doc, options, callback);
+  function insertUniquelyBody(doc, options, callback)
+  {
+    self.insert(doc, options, function(err, docs) {
+      if (err)
+      {
+        // 11000 is the MongoDB error code for attempting to
+        // insert two posts with the same slug (a unique 
+        // index error). When this happens, add a random
+        // digit to the slug and try again until it is unique
+        if ((err.code === 11000) && (err.err.indexOf('slug') !== -1))
+        {
+          doc.slug += (Math.floor(Math.random() * 10)).toString();
+          insertUniquelyBody(doc, options, callback);
+          return;
+        }
+      }
+      else
+      {
+        callback(err, docs);
+      }
+    });
+  }
 }
